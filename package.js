6cf97db8904f24bac52f6fffb36228194c3a7c9c
David Fudge <rkstar@mac.com>
2015-07-29 21:52:48 -0400

    added github url

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'A wrapper for the Buffer API',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/rkstar/buffer-api',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
