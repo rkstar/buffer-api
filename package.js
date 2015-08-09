8c408764597ed21b64423f521b130d026fe8cbae
David Fudge <rkstar@mac.com>
2015-08-08 21:14:05 -0400

    npmRequestOptions only works with POST method.  use params:{} for GET method calls.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'rkstar:buffer-api',
-  version: '1.0.2',
+  version: '1.0.3',
   // Brief, one-line summary of the package.
   summary: 'A wrapper for the Buffer API',
   // URL to the Git repository containing the source code for this package.
